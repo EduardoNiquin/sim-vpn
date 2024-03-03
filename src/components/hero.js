78ec5bed06c174637709f177684a9f74bab4c872
Eniquin <e.niquin.m99@gmail.com>
2024-03-03 19:38:32 -0300

    implementada funcionalidad para utilizar SVG como componentes

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,4 +1,7 @@
 import '../styles/hero.css';
+import CheckIcon from './svg/check.js';
+const checkIconHTML = CheckIcon({ fill: 'red', width: '0.9rem', height: '0.9rem' });
+
 const hero = `
 <div class="header custom-header">
     <!--Waves Container-->
@@ -8,11 +11,11 @@ const hero = `
         <div class="col">
                 <span>Navega con total libertad y privacidad.</span>
                 <h1>2 años de SimVPN por solo $2.19 al mes</h1>
-                <ul>
-                <li>Mantén tus datos seguros y tu actividad online privada con la VPN líder del mundo.</li>
-                <li>Oculta tu dirección IP y navega sin ser rastreado.</li>
-                <li>Protege tu información personal y financiera de hackers y malware.</li>
-                <li>Disfruta de contenido sin restricciones geográficas en cualquier país.</li>
+                <ul style="list-style-type: none;text-align: left;text-wrap: balance;padding-left: 0;">
+                    <li>${checkIconHTML}Mantén tus datos seguros y tu actividad online privada.</li>
+                    <li>${checkIconHTML}Oculta tu dirección IP y navega sin ser rastreado.</li>
+                    <li>${checkIconHTML}Protege tu información personal y financiera de hackers y malware.</li>
+                    <li>${checkIconHTML}Disfruta de contenido sin restricciones geográficas en cualquier país.</li>
                 </ul>
                 <button type="button" class="btn btn-primary">Obtener SimVPN ahora</button>
                 <p>Garantía de devolución de 30 días garantizada</p>
---
+++ src/components/svg/check.js
@@ -0,0 +1,6 @@
+
+function CheckIcon({ fill = 'black', stroke = 'none', width = '100', height = '100' }) {
+    return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="${width}" height="${height}"><path fill="${fill}" stroke="${stroke}" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>`;
+}
+
+export default CheckIcon;
